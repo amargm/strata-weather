02347da914d613969dce01c053e332aaa944704f
amargm <mugaliamar@gmail.com>
2026-05-01 11:34:11 +0530

    fix: add Expo modules autolinking for native module support
    - settings.gradle: add Expo autolinking + useExpoModules()
    - app/build.gradle: add Expo module dependencies + generated package list
    - MainApplication.kt: register ExpoModulesPackage
    
    Fixes: Cannot find native module 'ExpoFontLoader'

--- android/app/src/main/java/com/fieldweather/MainApplication.kt
+++ android/app/src/main/java/com/fieldweather/MainApplication.kt
@@ -10,6 +10,7 @@ import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint.load
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
+import expo.modules.ExpoModulesPackage
 
 class MainApplication : Application(), ReactApplication {
     override val reactNativeHost: ReactNativeHost =
@@ -17,6 +18,7 @@ class MainApplication : Application(), ReactApplication {
             override fun getPackages(): List<ReactPackage> =
                 PackageList(this).packages.apply {
                     // Add manually linked packages here
+                    add(ExpoModulesPackage())
                 }
 
             override fun getJSMainModuleName(): String = "index"
